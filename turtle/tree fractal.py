a8443151093bd72cdfb6db3103c55fa507b666e1
alicesophi <120331753+alicesophi@users.noreply.github.com>
2022-12-11 14:12:25 -0400

    Add files via upload

---
+++ turtle/tree fractal.py
@@ -0,0 +1,34 @@
+from turtle import *
+  
+speed('fastest')
+
+right(-90)
+  
+angle = 30
+  
+def tree(size, level):   
+  
+    if level > 0:
+        colormode(255)
+
+        pencolor(0, 255//level, 0)
+          
+        forward(size)
+  
+        right(angle)
+
+        tree(0.8 * size, level-1)
+          
+        pencolor(0, 255//level, 0)
+          
+        left( 2 * angle )
+
+        tree(0.8 * size, level-1)
+          
+        pencolor(0, 255//level, 0)
+          
+        right(angle)
+        forward(-size)
+           
+
+tree(80, 7)
